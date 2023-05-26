c890a2dc5c38bb43021edc1485016f8ce7a7e189
Danil <headcrabj1@gmail.com>
2023-05-27 00:01:17 +0500

    Fix 2

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 export default async function (instance, opts, done) {
     instance.get('/', async (req, res) => {
-        res.send({ ip: req.ip })
+        res.send({ ip: req.headers['x-forwarded-for'] })
     })
     
     done()
